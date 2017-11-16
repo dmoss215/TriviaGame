3405a06ca7aa7c6f8ed15a37826da10e8ff44ed8
Daniel Moss <dmoss@stratoswp.com>
2017-11-16 15:59:28 -0500

    start button, timer, and correct or incorrect working but needs work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,18 +3,38 @@ $(function() {
 
     let correct = 0;
     let wrong = 0;
+    let intervalId;
+    var number = 60;
 
+    // ============  Function to count down from 60 ======================================
+    function decrement() {
+        
+              number--;
+        
+              $("#show-number").html("<h2> Time: " + number + "</h2>");
+        
+              if (number === 0) {
+        
+                stop();
+        
+                alert("Time Up!");
+              }
+            }
+
+    function startClock() { 
+        intervalId = setInterval(decrement, 1000);
+     }
+
+    //  =================   Click start button to show questions ==================================
     $('.start-button').on('click', function() {
         var questionBox = document.getElementById("questions");
         questionBox.style.display = "block";
-        // if (questionBox.style.display === "none") {
-        //     questionBox.style.display = "block";
-        // } else {
-        //     questionBox.style.display = "none";
-        // }
+        startClock();
+        console.log(number);
+
     })
 
-
+    // ===================  Check each input for each question to see if value is correct or incorrect =====================
     $('#one input').on('change', function() {
         // alert($('input[name=one]:checked', '#one').val());
         if ($('input[name=one]:checked', '#one').val() === "correct") {
