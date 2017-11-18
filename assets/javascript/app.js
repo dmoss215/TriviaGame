02c76396963bc946af2013f658c352627008baf5
Daniel Moss <dmoss@stratoswp.com>
2017-11-17 20:11:25 -0500

    working except for game doesnt end when time's up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,6 +25,11 @@ $(function() {
         intervalId = setInterval(decrement, 1000);
      }
 
+     function stop() {
+              clearInterval(intervalId);
+            }
+        
+
     //  =================   Click start button to show questions ==================================
     $('.start-button').on('click', function() {
         var questionBox = document.getElementById("questions");
@@ -35,21 +40,98 @@ $(function() {
     })
 
     // ===================  Check each input for each question to see if value is correct or incorrect =====================
-    $('#one input').on('change', function() {
-        // alert($('input[name=one]:checked', '#one').val());
+    // $('#one input').on('change', function() {
+    //     // alert($('input[name=one]:checked', '#one').val());
+    //     if ($('input[name=one]:checked', '#one').val() === "correct") {
+    //         correct++;
+    //         console.log(correct);
+    //         console.log(wrong);
+    //     } else {
+    //         wrong++;
+    //         console.log(correct);
+    //         console.log(wrong);
+    //     }
+    //  });
+
+
+    $("#submit").on('click', function () { 
         if ($('input[name=one]:checked', '#one').val() === "correct") {
             correct++;
             console.log(correct);
-            console.log(wrong);
-        } else {
+        } else if ($('input[name=one]:checked', '#one').val() === "wrong") {
             wrong++;
-            console.log(correct);
             console.log(wrong);
         }
+
+        if ($('input[name=two]:checked', '#two').val() === "correct") {
+            correct++;
+            console.log(correct);
+        } else if ($('input[name=two]:checked', '#two').val() === "wrong") {
+            wrong++;
+            console.log(wrong);
+        }
+
+        if ($('input[type=radio]:checked', '#three').val() === "correct") {
+            correct++;
+            console.log(correct);
+        } else if ($('input[type=radio]:checked', '#three').val() === "wrong") {
+            wrong++;
+            console.log(wrong);
+        }
+
+        if ($('input[type=radio]:checked', '#four').val() === "correct") {
+            correct++;
+            console.log(correct);
+        } else if ($('input[type=radio]:checked', '#four').val() === "wrong") {
+            wrong++;
+            console.log(wrong);
+        }
+
+        if ($('input[type=radio]:checked', '#five').val() === "correct") {
+            correct++;
+            console.log(correct);
+        } else if ($('input[type=radio]:checked', '#five').val() === "wrong") {
+            wrong++;
+            console.log(wrong);
+        }
+
+        if ($('input[type=radio]:checked', '#six').val() === "correct") {
+            correct++;
+            console.log(correct);
+        } else if ($('input[type=radio]:checked', '#six').val() === "wrong") {
+            wrong++;
+            console.log(wrong);
+        }
+
+        if ($('input[type=radio]:checked', '#seven').val() === "correct") {
+            correct++;
+            console.log(correct);
+        } else if ($('input[type=radio]:checked', '#seven').val() === "wrong") {
+            wrong++;
+            console.log(wrong);
+        }
+
+        if ($('input[type=radio]:checked', '#eight').val() === "correct") {
+            correct++;
+            console.log(correct);
+        } else if ($('input[type=radio]:checked', '#eight').val() === "wrong") {
+            wrong++;
+            console.log(wrong);
+        }
+
+        
+        stop();
+
+        $('#questions').empty();
+
+        $('#correct-answers').append('Correct Answers:' + correct + '<br>');
+        $('#correct-answers').append('Wrong Answers:' + wrong);
+
+        console.log(correct, wrong);
      });
 
 
-
+      
 
 
 
